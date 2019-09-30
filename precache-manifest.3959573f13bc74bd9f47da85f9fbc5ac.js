2246a47a9b1f4737d7f52e043fbc9204522405a6
github username <github email address>
2019-09-29 20:31:45 -0400

    Updates

---
+++ precache-manifest.3959573f13bc74bd9f47da85f9fbc5ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15096caf8e35b71736dd775519a09a6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2ee664d577de7a31dddd",
+    "url": "/monsters-rolodex/static/css/main.8121b8e9.chunk.css"
+  },
+  {
+    "revision": "0e6f6fec45c2335e2d6e",
+    "url": "/monsters-rolodex/static/js/2.9462511a.chunk.js"
+  },
+  {
+    "revision": "2ee664d577de7a31dddd",
+    "url": "/monsters-rolodex/static/js/main.c581219a.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
